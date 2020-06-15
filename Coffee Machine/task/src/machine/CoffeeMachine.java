34ec89ce77a8a9c4687519abfa53684b5cb9b452
chopokmado <chopokmado@gmail.com>
2020-06-16 00:09:01 +0900

    스테이지 5. 루프를 돌리면서 액션을 수행하라. 루프를 돌린다. 재료를 채운다. 아무행동을 하지 않고 뒤로 돌아가기를 수행한다.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,33 +1,35 @@
 package machine;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         CoffeeMachineStatus coffeeMachineStatus = new CoffeeMachineStatus(400, 540, 120, 9, 550);
 
-        coffeeMachineStatus.printStatus();
+        while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            String line = sc.nextLine();
 
-        System.out.println("Write action (buy, fill, take):");
-        System.out.print(">");
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
+            switch (line) {
+                case "buy":
+                    coffeeMachineStatus.BuyAction();
+                    break;
+                case "fill":
+                    coffeeMachineStatus.FillAction();
+                    break;
+                case "take":
+                    coffeeMachineStatus.takeAction();
+                    break;
+                case "remaining":
+                    coffeeMachineStatus.printStatus();
+                    break;
+                case "exit":
+                    return;
 
-        switch (line) {
-            case "buy":
-                coffeeMachineStatus.BuyAction();
-                break;
-            case "fill":
-                coffeeMachineStatus.FillAction();
-                break;
-            case "take":
-                coffeeMachineStatus.takeAction();
-                break;
+            }
         }
-
-        coffeeMachineStatus.printStatus();
 
     }
 
--- Coffee Machine/task/src/machine/CoffeeMachineStatus.java
+++ Coffee Machine/task/src/machine/CoffeeMachineStatus.java
@@ -35,28 +35,51 @@ public class CoffeeMachineStatus {
 
     public void BuyAction() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        int selection = sc.nextInt();
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String selection = sc.nextLine();
 
         switch (selection) {
-            case 1:
+            case "1":
                 reduce(250, 0, 16, 4);
                 break;
-            case 2:
+            case "2":
                 reduce(350, 75, 20, 7);
                 break;
-            case 3:
+            case "3":
                 reduce(200, 100, 12, 6);
+                break;
+            case "back":
                 break;
         }
     }
 
     private void reduce(int water, int milk, int coffeeBeans, int money) {
-        this.water -= water;
-        this.milk -= milk;
-        this.coffeeBeans -= coffeeBeans;
+        int afterWater = this.water - water;
+        int afterMilk = this.milk - milk;
+        int afterCoffeeBeans = this.coffeeBeans - coffeeBeans;
+
+        if (afterWater < 0) {
+            System.out.println("Sorry, not enough water!");
+            return;
+        }
+
+        if (afterMilk < 0) {
+            System.out.println("Sorry, not enough milk!");
+            return;
+        }
+
+        if (afterCoffeeBeans < 0) {
+            System.out.println("Sorry, not enough coffee beans!");
+            return;
+        }
+
+        this.water = afterWater;
+        this.milk = afterMilk;
+        this.coffeeBeans = afterCoffeeBeans;
         this.money += money;
         this.cups -= 1;
+
+        System.out.println("I have enough resources, making you a coffee!");
     }
 
     public void FillAction() {
