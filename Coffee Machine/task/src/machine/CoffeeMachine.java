44ca72ebc736d9b076329dd16f2a31340e5d6b20
chopokmado <chopokmado@gmail.com>
2020-06-15 13:58:43 +0900

    스테이지 3. 물, 우유, 커피량을 받고 뽑고싶은 커피수를 결정한다. 주어진 물, 우유, 커피콩량으로 뽑고싶은 커피를 뽑을수 있는지 확인한다. 제공하고도 더 뽑을 수 있는 숫자도 계산한다.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,16 +1,54 @@
 package machine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
+        // 주어진 물의 양
+        System.out.println("Write how many ml of water the coffee machine has:");
         Scanner sc = new Scanner(System.in);
-        int data = sc.nextInt();
+        int water = sc.nextInt();
 
-        System.out.println(String.format("For %d cups of coffee you will need:", data));
-        System.out.println(String.format("%d ml of water", data * 200));
-        System.out.println(String.format("%d ml of milk", data * 50));
-        System.out.println(String.format("%d g of coffee beans", data * 15));
+        // 주어진 우유의 양
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = sc.nextInt();
+
+        // 주어진 커피콩 수
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int grams = sc.nextInt();
+
+        // 뽑고 싶은 커피의 갯수
+        System.out.println("Write how many cups of coffee you will need:");
+        int coffee = sc.nextInt();
+
+        int maximumCoffee = determineValues(water, milk, grams);
+
+        if (coffee <= maximumCoffee) {
+            if (coffee > 0) {
+                System.out.println(String.format("Yes, I can make that amount of coffee (and even %d more than that)", maximumCoffee - coffee));
+            } else {
+                System.out.println("Yes, I can make that amount of coffee");
+            }
+        } else {
+            System.out.println(String.format("No, I can make only %d cup(s) of coffee", maximumCoffee));
+        }
+
+
+
+        /*if (determineValues(water, milk, grams, coffee)) {
+            System.out.println("Yes, I can make that amount of coffee (and even 2 more than that)");
+        }*/
+
+    }
+
+    private static int determineValues(int water, int milk, int grams) {
+        int maxByWater = water / 200;
+        int maxByMilk = milk / 50;
+        int maxByGrams = grams / 15;
+
+        int[] arr = {maxByWater, maxByMilk, maxByGrams};
+        return Arrays.stream(arr).min().getAsInt();
+
     }
 }
