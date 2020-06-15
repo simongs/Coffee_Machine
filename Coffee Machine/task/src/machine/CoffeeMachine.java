d72c7100014e9c234ed68c9257e1b566e1ef6e3b
chopokmado <chopokmado@gmail.com>
2020-06-15 13:25:25 +0900

    스테이지 2, 커피 갯수의 입력을 받아서 물, 우유, 커피 용량을 계산한다.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,16 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+
+        System.out.println(String.format("For %d cups of coffee you will need:", data));
+        System.out.println(String.format("%d ml of water", data * 200));
+        System.out.println(String.format("%d ml of milk", data * 50));
+        System.out.println(String.format("%d g of coffee beans", data * 15));
     }
 }
